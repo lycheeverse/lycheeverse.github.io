3a47e7e874ede062052f5907752eb763fa850a9d
Jacob Lamb <jacobtlamb@hey.com>
2024-01-25 14:17:50 -0800

    Add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "lychee.github.io",
 	integrations: [
 		starlight({
 			title: "Docs",
