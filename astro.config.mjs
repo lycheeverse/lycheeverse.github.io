a9fffe8befec467117511aa33340a041ab03dd41
Matthias <matthias-endler@gmx.net>
2024-01-28 02:26:45 +0100

    Remove empty reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -136,10 +136,6 @@ export default defineConfig({
             },
           ],
         },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
       ],
     }),
   ],
