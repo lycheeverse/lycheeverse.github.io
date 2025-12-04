b81f0380f42ddf9a11b1e1073c73234357a2a67b
Thomas Zahner <thomas.zahner@protonmail.ch>
2025-12-04 09:51:08 +0100

    Redirect /introduction to / (#132)
    Co-authored-by: Tomodachi94 <tomodachi94@protonmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ export default defineConfig({
 			[smartypants, { dashes: false }],
 		],
 	},
+	redirects: {
+		"/introduction": "/overview/",
+	},
 	integrations: [
 		generateFilesFromMainRepo(),
 		starlight({
